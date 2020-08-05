868f6a105f5457d7a78fb99815a628279a176703
Aleksander Kapera <aleksanderkapera@gmail.com>
2020-08-05 12:15:06 +0200

    Adjusted recycler adapter for databinding

--- app/src/main/java/com/aleksanderkapera/covidstats/ui/adapter/LatestsStatsAdapter.kt
+++ app/src/main/java/com/aleksanderkapera/covidstats/ui/adapter/LatestsStatsAdapter.kt
@@ -1,21 +1,39 @@
 package com.aleksanderkapera.covidstats.ui.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
+import com.aleksanderkapera.covidstats.R
+import com.aleksanderkapera.covidstats.databinding.ViewLatestStatsBinding
+import com.aleksanderkapera.covidstats.domain.AllStatusStatistic
 
-class LatestStatsAdapter() : RecyclerView.Adapter<LatestStatsAdapter.ViewHolder>() {
+class LatestStatsAdapter(private val todayStats: List<LiveData<AllStatusStatistic>>) :
+    RecyclerView.Adapter<LatestStatsAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(parent)
+        val binding = ViewLatestStatsBinding.inflate(
+            LayoutInflater.from(
+                parent.context
+            ),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
     }
 
-    override fun getItemCount(): Int = 0
+    override fun getItemCount(): Int = todayStats.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(todayStats[position])
     }
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val itemBinding: ViewLatestStatsBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
 
+        fun bind(data: LiveData<AllStatusStatistic>) {
+            itemBinding.data = data
+        }
     }
 }
