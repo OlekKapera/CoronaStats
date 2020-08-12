108d8e1bdffcfbaea3ac1ecb0d980f19981698df
Aleksander Kapera <aleksanderkapera@gmail.com>
2020-08-12 15:15:17 +0200

    Replaced configure activity with transparent dialog

--- app/src/main/java/com/aleksanderkapera/covidstats/ui/LatestStatsWidgetConfigureActivity.kt
+++ app/src/main/java/com/aleksanderkapera/covidstats/ui/LatestStatsWidgetConfigureActivity.kt
@@ -5,16 +5,35 @@ import android.appwidget.AppWidgetManager
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.aleksanderkapera.covidstats.CovidStatsApp
 import com.aleksanderkapera.covidstats.R
-import kotlinx.android.synthetic.main.activity_latest_stats_widget_configure.*
+import com.aleksanderkapera.covidstats.repository.StatsRepository
+import com.aleksanderkapera.covidstats.room.StatsDatabase
+import com.aleksanderkapera.covidstats.util.asString
+import com.aleksanderkapera.covidstats.viewmodel.MainFragmentViewModel
+import com.aleksanderkapera.covidstats.viewmodel.MainFragmentViewModelFactory
 
 class LatestStatsWidgetConfigureActivity : AppCompatActivity() {
 
     private var appWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
+    private val database = StatsDatabase.getInstance(CovidStatsApp.context)
+    private val repository = StatsRepository.getInstance(database)
+    private lateinit var viewModel: MainFragmentViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_latest_stats_widget_configure)
+
+        viewModel = ViewModelProvider(
+            this,
+            MainFragmentViewModelFactory(repository)
+        ).get(MainFragmentViewModel::class.java)
+
+        ChooseCountryDialog(viewModel).show(
+            supportFragmentManager,
+            R.string.dialog_choose_country.asString()
+        )
 
         val extras = intent.extras
         appWidgetId = extras?.getInt(
@@ -22,13 +41,23 @@ class LatestStatsWidgetConfigureActivity : AppCompatActivity() {
             AppWidgetManager.INVALID_APPWIDGET_ID
         ) ?: AppWidgetManager.INVALID_APPWIDGET_ID
 
-        button.setOnClickListener {
-            val resultValue = Intent()
-            resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
-            setResult(RESULT_OK, resultValue)
-            finish()
-        }
+        viewModel.userCountries.observe(this, Observer {
+            viewModel.updateTodayStats()
+
+            if (viewModel.chooseCountryDialogEvent) {
+                viewModel.updateStats()
+                viewModel.finishCountryDialogChosen()
+                close()
+            }
+        })
 
         setResult(Activity.RESULT_CANCELED)
+    }
+
+    private fun close() {
+        val resultValue = Intent()
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId)
+        setResult(RESULT_OK, resultValue)
+        finish()
     }
 }
